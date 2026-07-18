a4d5672ab30dcff2b7218bec38d8e36472be0fcc
TahneeH8813 <tahnee.hopman@gmail.com>
2026-07-18 16:41:10 +1000

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var toggle = document.querySelector('.nav-toggle');
+  var nav = document.querySelector('.site-nav');
+  if (toggle && nav) {
+    toggle.addEventListener('click', function () {
+      nav.classList.toggle('open');
+      var expanded = nav.classList.contains('open');
+      toggle.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+    });
+  }
+});
